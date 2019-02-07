d767ad1a23cfc30ce565124c76740505547d400c
Joel Kniaz <joel.kniaz@gmail.com>
2019-02-07 12:45:33 -0500

    Adding draft flwor query API in new QueryAPIs, along with test input and output data files, new output DTOs, unit test, and an additional maven ISAAC dependency

---
+++ src/main/java/net/sagebits/tmp/isaac/rest/api1/data/query/RestQueryResults.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2019 VetsEZ Inc, Sagebits LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Contributions from 2015-2017 were performed either by US government
+ * employees, or under US Veterans Health Administration contracts.
+ *
+ * US Veterans Health Administration contributions by government employees
+ * are work of the U.S. Government and are not subject to copyright
+ * protection in the United States. Portions contributed by government
+ * employees are USGovWork (17USC §105). Not subject to copyright.
+ * 
+ * Contribution by contractors to the US Veterans Health Administration
+ * during this period are contractually contributed under the
+ * Apache License, Version 2.0.
+ *
+ * See: https://www.usa.gov/government-works
+ */
+
+package net.sagebits.tmp.isaac.rest.api1.data.query;
+
+import java.util.Arrays;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * 
+ * {@link RestQueryResults}
+ *
+ * @author <a href="mailto:joel.kniaz.list@gmail.com">Joel Kniaz</a>
+ */
+@XmlRootElement
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
+public class RestQueryResults
+{
+	private static Logger log = LogManager.getLogger();
+
+	@XmlElement(name="row")
+	@JsonInclude(JsonInclude.Include.ALWAYS)
+	RestQueryResultsRow[] rows;
+
+	protected RestQueryResults()
+	{
+		// for Jaxb
+	}
+
+	public RestQueryResults(RestQueryResultsRow...rows) {
+		this.rows = rows;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(rows);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RestQueryResults other = (RestQueryResults) obj;
+		if (!Arrays.equals(rows, other.rows))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "RestQueryResults [\n\trows=" + Arrays.toString(rows) + "\n]";
+	}
+}
